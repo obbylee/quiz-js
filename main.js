3654648e4968d348d48e03732ecd517a2fd2a10b
Lee Obby <leeobbyw@gmail.com>
2020-12-14 19:46:17 +0700

    Complete task

--- main.js
+++ main.js
@@ -1,39 +1,117 @@
-function makeRequest(location) {
-    return new Promise((accept, decline) => {
-        console.log(`Making request to ${location}`);
-        if (location === 'Google') {
-            accept('Google Say Hi!')
-        } else {
-            decline('We can only talk to Google')
+const startBtn = document.querySelector('#start-btn');
+const nextBtn = document.querySelector('#next-btn');
+const questionContainer = document.querySelector('#question-container');
+const question = document.querySelector('#question');
+const answerBtn = document.querySelector('#answer-btn');
+
+let shuffleQ, currentQ;
+
+const questionsData = [
+    {
+        question: 'What is 2 + 2',
+        answer: [
+            {text: '4', correct: true},
+            {text: '22', correct: false}
+        ]
+    },
+    {
+        question: 'What is 1000 * 2',
+        answer: [
+            {text: '7', correct: false},
+            {text: '9', correct: false},
+            {text: '2000', correct: true},
+            {text: '10', correct: false}
+        ]
+    },
+    {
+        question: 'Would you like vanilla ice cream?',
+        answer: [
+            {text: 'YES!!!', correct: true},
+            {text: 'NO', correct: false}
+        ]
+    },
+    {
+        question: 'What is your best quality?',
+        answer: [
+            {text: 'Gaming', correct: true},
+            {text: 'Coding', correct: false},
+            {text: 'Everything', correct: false}
+        ]
+    }
+];
+
+startBtn.addEventListener('click', startGame);
+nextBtn.addEventListener('click', () => {
+    currentQ++;
+    nextQuestion();
+});
+
+function startGame() {
+    console.log('Started');
+    startBtn.classList.add('hide');
+    questionContainer.classList.remove('hide');
+
+    shuffleQ = questionsData.sort(() => Math.random() - .5);
+    currentQ = 0;
+
+    nextQuestion();
+}
+
+function nextQuestion() {
+    resetState();
+    showQuestion(shuffleQ[currentQ]);
+}
+
+function showQuestion(questionsData) {
+    // console.log(questionsData.answer.text[0]);
+    question.innerText = questionsData.question;
+    questionsData.answer.forEach(answerText => {
+        const button = document.createElement('button');
+        button.innerText = answerText.text;
+        button.classList.add('btn');
+        if (answerText.correct) {
+            button.dataset.correct = answerText.correct;
         }
-    })
+        button.addEventListener('click', pickAnswer);
+        answerBtn.appendChild(button);
+    });
 }
 
-function processRequest(response) {
-    return new Promise((accept, decline) => {
-        console.log('Processing Request');
-        accept(`Extra information ${response}`)
-    })
-}
-
-// makeRequest('Google').then(response => {
-//     console.log(`Response recieved`);
-//     return processRequest(response);
-// }).then(processingRequest => {
-//     console.log(processingRequest);
-// }).catch( err => {
-//     console.log(err);
-// })
-
-async function doWork() {
-    try {
-        const response = await makeRequest('Facebook')
-        console.log('Response recieved');
-        const processedRequest = await processRequest(response)
-        console.log(processedRequest);
-    } catch (err) {
-        console.log(err);
+function resetState() {
+    clearStatusClass(document.body);
+    nextBtn.classList.add('hide');
+    // console.log(answerBtn.firstChild);
+    while(answerBtn.firstChild) {
+        answerBtn.removeChild(answerBtn.firstChild);
     }
 }
 
-doWork()
+function pickAnswer(e) {
+    const selectButton = e.target;
+    const correct = selectButton.dataset.correct;
+    setStatusClass(document.body, correct);
+    Array.from(answerBtn.children).forEach(btn => {
+        setStatusClass(btn, btn.dataset.correct);
+    });
+    if (shuffleQ.length > currentQ + 1) {
+        nextBtn.classList.remove('hide');
+    } else {
+        startBtn.innerText = 'Restart';
+        startBtn.classList.remove('hide');
+    }
+    
+}
+
+function setStatusClass(element, correct) {
+    clearStatusClass(element);
+    if (correct) {
+        element.classList.add('correct');
+    } else {
+        element.classList.add('wrong');
+    }
+}
+
+function clearStatusClass(element) {
+    element.classList.remove('correct');
+    element.classList.remove('wrong');
+}
